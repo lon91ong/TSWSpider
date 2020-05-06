206140afb20c62cc56d4631ad5e52a748daf6586
lon91ong <lon91ong@gmail.com>
2020-05-06 21:37:21 +0800

    小于200章出错

--- test.py
+++ test.py
@@ -4,20 +4,24 @@ import requests
 from time import time, sleep
 from random import choice
 from json import loads
-from pprint import pprint
+#from urllib.parse import quote
 
-for rl in range(1,159):
-    ref_str = "https://www.ting22.com/ting/1688-"+str(rl)+"1.html"
-    cookie_str = "shistory=think%3A%5B%22%25E5%258F%25B2%25E4%25B8%258A%25E6%259C%2580%25E5%25BC%25BA%25E8%25B5%2598%25E5%25A9%25BF%22%5D; PHPSESSID=atutpbhgs4h1gdsb3gjpncdkn6; 1688_setNAME=%E5%8F%B2%E4%B8%8A%E6%9C%80%E5%BC%BA%E8%B5%98%E5%A9%BF(%E9%AB%98%E6%99%BA%E5%95%86%E8%A3%85X%E6%89%93%E8%84%B8)%20%E7%AC%AC721%E7%AB%A0; 1688_setURL=" + ref_str + "; index_setID=1688"
-    cookie = {i.split("=")[0]: i.split("=")[1] for i in cookie_str.split("; ")}
-    sign = str(int(round(time() * 1000)))  # 时间戳
+novelID = '1688'
+novelName = '史上最强赘婿'
+#cookie = {'PHPSESSID':'atutpbhgs4h1gdsb3gjpncdkn6'}  # 这个需要手动提取
+
+for rl in range(19,22):
+    ref_str = "https://www.ting22.com/ting/{}-{}.html".format(novelID,str(rl))
+    #cookie['shistory'] = quote('think:[{}]'.format(quote(novelName)))
+    #cookie[novelID+'_setURL'] = ref_str
+    #cookie['index_setID'] = novelID
+    sign = int(round(time() * 1000))  # 时间戳
     
     headers = {
         "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
         "Accept-encoding": "gzip, deflate, br",
         "Accept-language": "zh-CN,zh;q=0.9,zh-TW;q=0.8",
         "X-Requested-With": "XMLHttpRequest",
-        # "Accept": "application/json",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
         "Referer": ref_str,
         "Sign": sign,
@@ -25,33 +29,35 @@ for rl in range(1,159):
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin"
     }
-    
+    getstr = "https://www.ting22.com/api.php?c=Json&id="+novelID+"&page="+str(rl+1)+"&pagesize=10&callback=jQuery21403942595757035292_{}&_={}".format(sign, sign+1)
+    print('GET:',getstr)
     response = requests.get(
-        "https://www.ting22.com/api.php?c=Json&id=1688&page="+str(rl+1)+"&pagesize=10&callback=jQuery21403942595757035292_{}&_={}".format(sign, sign), headers=headers)
+        getstr, headers=headers)
     
     json_str = response.text
     json_str = json_str.encode('raw_unicode_escape').decode('raw_unicode_escape').replace(r'\/\/', '//').replace(r'\/', '/')
     #print('jsonStr:',json_str[41:-2])
     json_data = loads(json_str[json_str.index('({')+1:-2])
-    
+    #cookie[novelID+'_setNAME'] = quote(novelName)+'('+quote(json_data["playlist"][0]["trackName"].split('(')[1][:-1])+')'+quote(' 第{}章'.format(rl*10+10))
     mp4_url_list = [x["file"] for x in json_data["playlist"]]
+    #print(json_data["playlist"][0]["trackName"].split('(')[1][:-1])
     name_list = [x["pid"] for x in json_data["playlist"]]
     mp4_real_url_list = [''.join(map(chr, [int(i) for i in s.split("*")])) for s in mp4_url_list]
     
-    #pprint(mp4_real_url_list)
-    pprint(name_list)
-    
+    print(mp4_real_url_list)
+    print(name_list[0])
+    ''' # 下载
     for i in range(len(mp4_url_list)):
         mp4 = requests.get(mp4_real_url_list[i])
-        with open('./史上最强赘婿/'+str(name_list[i])+'.mp3',"wb") as f:
+        with open('./'+novelName+'/'+str(name_list[i])+'.mp3',"wb") as f:
             f.write(mp4.content)
             f.close()
-            print('{}.mp3 下载完成！'.format(name_list[i]))
-            
+            print('{}.mp3 -- 下载完成！'.format(name_list[i]))
+    '''
     sleep(choice([0.3, 0.5, 0.8, 1.1, 1.5, 1.8, 2, 2.3, 2.5]))
 
 import os
-print(os.listdir("./史上最强赘婿"))
+#print(os.listdir("./"+novelName))
 
 def get_dir_size(dir_path):
     file_list = os.listdir(dir_path)
@@ -60,4 +66,4 @@ def get_dir_size(dir_path):
         size += os.path.getsize(dir_path + "/" + file_name)
     return round(size / 1024 / 1024, 2)
 
-print('{} MB'.format(get_dir_size("./史上最强赘婿")))
+print('{} MB'.format(get_dir_size("./"+novelName)))
